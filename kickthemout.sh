0fffdc0636f871acfa8385f5663e84c1759473b1
tobyw121 <tobiasw763@gmail.com>
2021-06-06 08:36:37 +0200

    kKickthemout auto installer (Script)

---
+++ kickthemout.sh
@@ -0,0 +1,9 @@
+sudo apt-get install nmap
+sudo apt-get install git
+sudo apt-get install pip
+git clone https://github.com/k4m4/kickthemout.git
+cd kickthemout/
+sudo -H pip3 install -r requirements.txt
+sudo python3 kickthemout.py -a
+
+
