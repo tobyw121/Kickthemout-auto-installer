16eca64a3512c31aead504174fd7db2bd8cf6b5a
tobyw121 <tobiasw763@gmail.com>
2021-06-06 08:48:49 +0200

    kickthemout.sh

--- kickthemout.sh
+++ kickthemout.sh
@@ -4,6 +4,6 @@ sudo apt-get install pip
 git clone https://github.com/k4m4/kickthemout.git
 cd kickthemout/
 sudo -H pip3 install -r requirements.txt
-sudo python3 kickthemout.py -a
+sudo python3 kickthemout.py -s -a
 
 
