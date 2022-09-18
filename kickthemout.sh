015c0d7de619192b6af5b9c53ee44e76a339e7d5
Toby <tobiasw763@gmail.com>
2022-09-18 15:05:42 +0200

    Update kickthemout.sh

--- kickthemout.sh
+++ kickthemout.sh
@@ -1,8 +1,9 @@
 sudo apt-get install nmap
 sudo apt-get install git
 sudo apt-get install pip
+cd /~
 git clone https://github.com/k4m4/kickthemout.git
-cd kickthemout/
+cd ~/kickthemout/
 sudo -H pip3 install -r requirements.txt
 sudo python3 kickthemout.py -s -a
 
